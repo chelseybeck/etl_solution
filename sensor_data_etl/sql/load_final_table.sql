f19fae40bccd7e40bfb385d9b4577248e030f3e1
opensav <reallygoodgirl@me.com>
2023-01-16 13:21:25 -0800

    adding interpolate and matching tasks and updating logic and documentation

--- sensor_data_etl/sql/load_final_table.sql
+++ sensor_data_etl/sql/load_final_table.sql
@@ -1,3 +1,5 @@
+-- changes order of columns and creates a 
+-- unique identifier for transformed rows
 SELECT  
 time,
 fx_1,
@@ -12,7 +14,21 @@ y_1,
 y_2,
 z_1,
 z_2,
-record_hash_code,
-etl_update_ts,
+TO_HEX(SHA256(TO_JSON_STRING(STRUCT( time,
+          fx_1,
+          fx_2,
+          fy_1,
+          fy_2,
+          fz_1,
+          fz_2,
+          x_1,
+          x_2,
+          y_1,
+          y_2,
+          z_1,
+          z_2, 
+          run_uuid )))) AS trns_record_hash_code,
+  -- creates unique identifier
+CURRENT_TIMESTAMP AS etl_update_ts,
 run_uuid
 FROM {{ params.source_table }}
--- sensor_data_etl/trns_sensor_data.py
+++ sensor_data_etl/trns_sensor_data.py
@@ -38,55 +38,77 @@ schedule_interval=schedule_interval) as dag:
 
     # Run transformation to clean data.
     # deduplicate, cast datatypes, and trim
-    clean_data = BigQueryOperator(
-        task_id="clean_data",
-        sql = 'sql/clean_data.sql',
+    clean_format = BigQueryOperator(
+        task_id="clean_format",
+        sql = 'sql/clean_format.sql',
         params = {"raw_table":f'{project_id}.{raw_src_dataset}.sensor_data'},
         destination_dataset_table = f'{project_id}.{etl_dataset}.w_sensor_data_clean',
         create_disposition = "CREATE_IF_NEEDED",
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    clean_data.set_upstream(start_task)
+    clean_format.set_upstream(start_task)
 
     # Convert timeseries to features by robot_id
     convert_to_features = BigQueryOperator(
         task_id="convert_to_features",
         sql = 'sql/convert_to_features.sql',
-        params={"clean_table":f'{project_id}.{etl_dataset}.w_sensor_data_clean'},
+        params={"source_table":f'{project_id}.{etl_dataset}.w_sensor_data_clean'},
         destination_dataset_table = f'{project_id}.{etl_dataset}.w_features_converted',
         create_disposition = "CREATE_IF_NEEDED",
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    convert_to_features.set_upstream(clean_data)
+    convert_to_features.set_upstream(clean_format)
 
     # Using dummy operators for tasks not yet written
-    # Match timestamps with measurements - TO DO
-    match_timestamps = DummyOperator(task_id='match_timestamps')
-    match_timestamps.set_upstream(convert_to_features)
+    # Match timestamps with measurements - DONE
+    match_values_timestamps = BigQueryOperator(
+        task_id="match_values_timestamps",
+        sql = 'sql/match_values_timestamps.sql',
+        params = {"source_table": f'{project_id}.{etl_dataset}.w_features_converted'},
+        destination_dataset_table = f'{project_id}.{etl_dataset}.w_timestamps_matched',
+        create_disposition = "CREATE_IF_NEEDED",
+        write_disposition = "WRITE_TRUNCATE",
+        use_legacy_sql=False 
+    )
+
+    match_values_timestamps.set_upstream(convert_to_features)
+
+    interpolate_values = BigQueryOperator(
+        task_id="interpolate_values",
+        sql = 'sql/interpolate_values.sql',
+        # Change source table below once rest of features are added
+        params = {"source_table": f'{project_id}.{etl_dataset}.w_timestamps_matched'},
+        destination_dataset_table = f'{project_id}.{etl_dataset}.w_values_interpolated',
+        create_disposition = "CREATE_IF_NEEDED",
+        write_disposition = "WRITE_TRUNCATE",
+        use_legacy_sql=False 
+    )
+
+    interpolate_values.set_upstream(match_values_timestamps)
 
     # Add engineered/calculated features - TO DO
     add_calculated_features = DummyOperator(task_id='add_calculated_features')
-    add_calculated_features.set_upstream(match_timestamps)
+    add_calculated_features.set_upstream(match_values_timestamps)
 
     # Calculate Runtime Statistics - TO DO
     calculate_runtime_stats = DummyOperator(task_id='calculate_runtime_stats')
-    calculate_runtime_stats.set_upstream(match_timestamps)
+    calculate_runtime_stats.set_upstream(match_values_timestamps)
 
     # Load final table
-    load_final_table = BigQueryOperator(
-        task_id="load_final_table",
+    load_base_table = BigQueryOperator(
+        task_id="load_base_table",
         sql = 'sql/load_final_table.sql',
         # Change source table below once rest of features are added
-        params = {"source_table": f'{project_id}.{etl_dataset}.w_features_converted'},
+        params = {"source_table": f'{project_id}.{etl_dataset}.w_values_interpolated'},
         destination_dataset_table = f'{project_id}.{target_dataset}.trns_sensor_data',
         create_disposition = "CREATE_IF_NEEDED",
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
 
-    load_final_table.set_upstream(match_timestamps)
+    load_base_table.set_upstream(match_values_timestamps)
 
     # Export table as csv to bucket
     # create GH action to download the file back to GH repo
