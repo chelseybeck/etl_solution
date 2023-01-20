825379b2a08255d38bd092ea1a8a0be61f09e5a6
opensav <reallygoodgirl@me.com>
2023-01-20 14:20:26 -0800

    simplifying logic

--- sensor_data_etl/sql/calculate_velocity.sql
+++ sensor_data_etl/sql/calculate_velocity.sql
@@ -44,15 +44,9 @@
     ABS(z_2 - z_2_prev_value) AS z_2_displacement
   FROM
     prev_value 
-  ),
-  velocity AS (SELECT
+  )
+  SELECT
     time,
-    x_1_displacement,
-    y_1_displacement,
-    z_1_displacement,
-    x_2_displacement,
-    y_2_displacement,
-    z_2_displacement,
     -- calculate velocity displacement / change in time
     -- displacement to time is millimeters to milliseconds
     -- divide each by 1000 to convert to meters per seconds
@@ -62,30 +56,7 @@
     ((x_2_displacement / 1000) / (change_in_time / 1000)) AS vx_2,
     ((y_2_displacement / 1000) / (change_in_time / 1000)) AS vy_2,
     ((z_2_displacement / 1000) / (change_in_time / 1000)) AS vz_2,
-    run_uuid,
-    trns_record_hash_code
-  FROM
-    changes
-  ORDER BY
-    time)
--- calculate totals
-SELECT 
-time,
-    vx_1,
-    SUM(vx_1) OVER win1 AS total_vx_1,
-    vy_1,
-    SUM(vy_1) OVER win1 AS total_vy_1,
-    vz_1,
-    SUM(vz_1) OVER win1 AS total_vz_1,
-    vx_2,
-    SUM(vx_2) OVER win1 AS total_vx_2,
-    vy_2,
-    SUM(vy_2) OVER win1 AS total_vy_2,
-    vz_2,
-    SUM(vz_2) OVER win1 AS total_vz_2,
-    -- displacement columns were added in the SELECT here because
-    -- we need to pull from these columns to calculate runtime stats
-    -- in another task
+    -- displacement columns are in the select here because they are used to calculate distance for runtime stats
     x_1_displacement,
     y_1_displacement,
     z_1_displacement,
@@ -93,15 +64,8 @@ time,
     y_2_displacement,
     z_2_displacement,
     run_uuid,
-    trns_record_hash_code,
-    CURRENT_TIMESTAMP AS etl_update_ts
-FROM velocity
-WINDOW
-    win1 AS (
-    PARTITION BY
-      run_uuid
-    ORDER BY
-      time ROWS BETWEEN UNBOUNDED PRECEDING
-      AND CURRENT ROW )
+    trns_record_hash_code
+  FROM
+    changes
   ORDER BY
-  time 
+    time
