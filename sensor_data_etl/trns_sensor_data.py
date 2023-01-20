9f7818194600116a9e1709cc937448f9696ffa2e
opensav <reallygoodgirl@me.com>
2023-01-20 14:22:29 -0800

    removing comments and unnecessary tasks

--- sensor_data_etl/trns_sensor_data.py
+++ sensor_data_etl/trns_sensor_data.py
@@ -48,7 +48,6 @@ schedule_interval=schedule_interval) as dag:
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # clean_format.set_upstream(start_task)
 
     # Convert timeseries to features by robot_id
     convert_to_features = BigQueryOperator(
@@ -60,7 +59,6 @@ schedule_interval=schedule_interval) as dag:
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # convert_to_features.set_upstream(clean_format)
 
     # Match timestamps with measurements 
     match_values_timestamps = BigQueryOperator(
@@ -73,8 +71,6 @@ schedule_interval=schedule_interval) as dag:
         use_legacy_sql=False 
     )
 
-    # match_values_timestamps.set_upstream(convert_to_features)
-
     # Interpolate null values between rows
     interpolate_values = BigQueryOperator(
         task_id="interpolate_values",
@@ -85,7 +81,6 @@ schedule_interval=schedule_interval) as dag:
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # interpolate_values.set_upstream(match_values_timestamps)
 
     # Calculate Velocity
     calculate_velocity = BigQueryOperator(
@@ -97,7 +92,6 @@ schedule_interval=schedule_interval) as dag:
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # calculate_velocity.set_upstream(interpolate_values)
 
     # Calculate Acceleration
     calculate_acceleration = BigQueryOperator(
@@ -109,19 +103,6 @@ schedule_interval=schedule_interval) as dag:
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # calculate_velocity.set_upstream(calculate_velocity)
-
-    # Calculate Force
-    calculate_force = BigQueryOperator(
-        task_id="calculate_force",
-        sql = 'sql/calculate_force.sql',
-        params = {"source_table": f'{project_id}.{target_dataset}.trns_base_values'},
-        destination_dataset_table = f'{project_id}.{etl_dataset}.w_force_calculated',
-        create_disposition = "CREATE_IF_NEEDED",
-        write_disposition = "WRITE_TRUNCATE",
-        use_legacy_sql=False 
-    )
-    # calculate_force.set_upstream(interpolate_values)
 
     # Combine Totals
     combine_totals = BigQueryOperator(
@@ -130,14 +111,13 @@ schedule_interval=schedule_interval) as dag:
         params = {
             "velocity_table": f'{project_id}.{etl_dataset}.w_velocity_calculated',
             "acceleration_table": f'{project_id}.{etl_dataset}.w_acceleration_calculated',
-            "force_table": f'{project_id}.{etl_dataset}.w_force_calculated'
+            "base_table": f'{project_id}.{target_dataset}.trns_base_values'
         },
         destination_dataset_table = f'{project_id}.{target_dataset}.trns_calculated_features',
         create_disposition = "CREATE_IF_NEEDED",
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # combine_totals.set_upstream([calculate_acceleration, calculate_force])
 
     # Summarize Totals
     summarize_totals = BigQueryOperator(
@@ -149,7 +129,6 @@ schedule_interval=schedule_interval) as dag:
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # summarize_totals.set_upstream(combine_totals)
 
     # Calculate Runtime Statistics
     calculate_runtime_stats = BigQueryOperator(
@@ -164,13 +143,12 @@ schedule_interval=schedule_interval) as dag:
         write_disposition = "WRITE_TRUNCATE",
         use_legacy_sql=False 
     )
-    # calculate_runtime_stats.set_upstream([calculate_velocity])
+
     divider = DummyOperator(task_id='divider')
     end_task = DummyOperator(task_id='end')
-    # end_task.set_upstream([calculate_runtime_stats, summarize_totals])
 
     # Define the order in which the tasks complete
-    chain(start_task, clean_format, convert_to_features, match_values_timestamps, interpolate_values, [calculate_velocity, calculate_force], divider, [calculate_acceleration, calculate_runtime_stats], combine_totals, summarize_totals, end_task) 
+    chain(start_task, clean_format, convert_to_features, match_values_timestamps, interpolate_values, calculate_velocity,[calculate_acceleration, calculate_runtime_stats], combine_totals, summarize_totals, end_task) 
    
    
     # start_task >> clean_format >> convert_to_features >> match_values_timestamps >> interpolate_values
